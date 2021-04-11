3faf3bfbf68e8c5c6642e6993cb8fdb1d7f6542d
Ibrahim-Aldereni <ibrahemaldrynee@gmail.com>
2021-04-11 16:41:43 +0300

    update app.js, finished fifth object

--- js/app.js
+++ js/app.js
@@ -284,3 +284,75 @@ Paris.generateRandomNum();
 Paris.NumOfCookies();
 Paris.CookiesSum();
 Paris.dispaly();
+
+/**************************** 5th object  ***********************/
+
+let Lima = {
+  location: 'Lima',
+  minCust: 2,
+  maxCust: 16,
+  avgCookies: 4.6,
+  WorkingHours:[],
+  randomCust: [],
+  CookiesNum:[],
+  TotalCookies: 0,
+
+  generateHours: function (start, hours) { 
+    while(this.WorkingHours.length < hours){
+      this.WorkingHours.push(start++);
+      if(start > 12){
+        start = 1;
+      }
+    } 
+  },
+  generateRandomNum: function() {  
+    for(let i=0; i<this.WorkingHours.length; i++){
+      this.randomCust.push(Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust));
+    }
+  },
+  NumOfCookies: function () {
+    let N = 0;
+    for(let i=0; i < this.randomCust.length; i++){
+      N = Math.ceil(this.avgCookies * this.randomCust[i]);
+      this.CookiesNum.push(N);
+    }
+  },
+  CookiesSum: function(){
+    for(let i=0; i < this.CookiesNum.length; i++){
+      this.TotalCookies += this.CookiesNum[i];
+    }
+  },
+  dispaly: function(){
+    let parent = document.querySelector('main');
+    let section = document.createElement('section');
+    section.setAttribute('id',`${this.location}`);
+    parent.appendChild(section);
+
+    let h2 = document.createElement('h2');
+    h2.textContent = this.location;
+    section.appendChild(h2);
+
+    let ul = document.createElement('ul');
+    let li = null;
+    let text = 'am';
+    for(let i=0; i < this.WorkingHours.length; i++){
+      li = document.createElement('li');
+      if(this.WorkingHours[i] === 12){
+        text = 'pm';
+      }
+        li.textContent = `${this.WorkingHours[i]}${text}: ${this.CookiesNum[i]} cookies`;
+        ul.appendChild(li);
+    }
+
+    li = document.createElement('li');
+    li.textContent = `Total: ${this.TotalCookies} cookies`;
+    ul.appendChild(li);
+    section.appendChild(ul);
+  }
+}
+Lima.generateHours(6,14);
+Lima.generateRandomNum();
+Lima.NumOfCookies();
+Lima.CookiesSum();
+Lima.dispaly();
+
